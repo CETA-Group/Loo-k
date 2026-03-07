1af60f9acc763c9e4e61fd18d9b7affd3f5f4e9a
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-03-07 11:32:29 -0500

    added backend auth0

---
+++ backend/auth0.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer
+from jose import jwt
+import requests
+
+AUTH0_DOMAIN = "dev-s6ofaf4udt2w1nw1.us.auth0.com"
+API_AUDIENCE = "https://trurent/api"
+ALGORITHMS = ["RS256"]
+
+token_auth_scheme = HTTPBearer()
+
+
+def verify_jwt(token=Depends(token_auth_scheme)):
+    token = token.credentials
+    jwks_url = f"https://{AUTH0_DOMAIN}/.well-known/jwks.json"
+    jwks = requests.get(jwks_url).json()
+    unverified_header = jwt.get_unverified_header(token)
+
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"],
+            }
+
+    if not rsa_key:
+        raise HTTPException(status_code=401, detail="Invalid header")
+
+    try:
+        payload = jwt.decode(
+            token,
+            rsa_key,
+            algorithms=ALGORITHMS,
+            audience=API_AUDIENCE,
+            issuer=f"https://{AUTH0_DOMAIN}/",
+        )
+        return payload
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+
+
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from backend.auth0 import verify_jwt
+from fastapi import Depends
 
 app = FastAPI()
 
@@ -10,3 +12,8 @@ class Item(BaseModel):
 @app.post("/items")
 def create_item(item: Item):
     return {"message": f"Received {item.name} with value {item.value}"}
+
+
+@app.get("/protected")
+def protected_route(user=Depends(verify_jwt)):
+    return {"message": "OK", "user": user}
